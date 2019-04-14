a78885e7f147a4ab7be7ff662ffe122b69a6eb4c
orelmuseri1 <orelmuseri1@gmail.com>
2019-04-15 00:58:37 +0300

    fix bug with the chack time func and add query about the amount of food per day

--- AskingQueriesFP/src/AskingQueriesFP/Asker.java
+++ AskingQueriesFP/src/AskingQueriesFP/Asker.java
@@ -38,13 +38,14 @@ public class Asker {
 		//sender.send(12,"10"+"/"+"4"+"/"+"2019","10"+":"+"12"+ ":" + "05","לא תקין","Water", "the kid doesn't drink enought today"); 	// the function that makes the alert
 		int countermsg = 0; //counting how match msg he send to the system in this run
 		myConn = DriverManager.getConnection("jdbc:mysql://localhost:" + this.port + "/FinelProjectDB", this.userName, this.password);			
+		countermsg += DailyFoodChack(myConn,WhatIsTheDate());
 		if(checkTime(MIDLLEHOUR,MIDLLEMIN,MIDLLEHOUR,MIDLLEMIN+3)) {
-			countermsg += DailyWaterChack(myConn,1); 
+			countermsg += DailyWaterChack(myConn,1,WhatIsTheDate()); 
 			}
 		if(checkTime(FINELHOUR,FINELMIN,FINELHOUR,FINELMIN+3)) {
-			countermsg += DailyWaterChack(myConn,2);
+			countermsg += DailyWaterChack(myConn,2,WhatIsTheDate());
 			if(!WhatIsTheDay().equals("Mon")||!WhatIsTheDay().equals("Sun"))
-			countermsg += XDaysEgoWaterChack(myConn);
+			countermsg += XDaysEgoWaterChack(myConn,WhatIsTheDate());
 			 }
 			//countermsg+= firstquery(myConn);
 		return countermsg;
@@ -53,20 +54,14 @@ public class Asker {
 		
 	
 	 // check the amount of water every child drink in the end of the day and the midlle of the day
-	int DailyWaterChack(Connection myConn, int time) {
+	int DailyWaterChack(Connection myConn, int time,String[] today) {
 		int value=0;
 		try {
 			Statement mystmt = myConn.createStatement();
 			String giveMeAllKids= "SELECT * FROM child";
 			ResultSet kids= mystmt.executeQuery(giveMeAllKids);//sent the query to get all the kids
-			String [] today = LocalDate.now().toString().split("-");
-			String[] dateEvent,temp,nowtime;
-			int counterWater = 0,hour,min,sec;
-			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-			nowtime = timeStamp.split("_");									//getting the date and the time of the event a
-			 hour = (nowtime[1].charAt(0)-'0')*10 +nowtime[1].charAt(1)-'0';
-			 min = (nowtime[1].charAt(2)-'0')*10 +nowtime[1].charAt(3)-'0';
-			 sec = (nowtime[1].charAt(4)-'0')*10 +nowtime[1].charAt(5)-'0';
+			String[] dateEvent,temp;
+			int counterWater = 0;
 			while(kids.next()) {																			// pass all the kids 
 				//System.out.println(kids.getString("firstName") + "," + kids.getString("childID"));          // print the name of the kid
 				String giveMeAllEvents = "SELECT * FROM Water WHERE child = " + kids.getString("childID"); 	//the query to get all the event in Water for this kid
@@ -76,13 +71,13 @@ public class Asker {
 					dateEvent = events.getString("eventDate").split("/");									//getting the date and the time of the event a
 					temp = dateEvent[2].toString().split(",");
 					dateEvent[2]=temp[0];
-					if(today[2].equals(dateEvent[0])&&today[1].equals(dateEvent[1])&& today[0].equals(dateEvent[2])) { //if the date of the event is today
+					if(today[0].equals(dateEvent[0])&&today[1].equals(dateEvent[1])&& today[2].equals(dateEvent[2])) { //if the date of the event is today
 						//System.out.println(events.getString("child")+"-"+ events.getString("amount") + "-" + events.getString("consumedAmount") + "-" + events.getString("eventDate")); //print the id of the kid the amount of water he get and the amount he actualy drink
 						if(events.getString("consumedAmount").equals("finish"))									//checking the amount he drink and sum the amount he drink all day
 							counterWater += events.getInt("amount");
-						else if(events.getString("consumedAmount").equals("more than helf"))
+						else if(events.getString("consumedAmount").equals("more than half"))
 							counterWater += 0.6*events.getInt("amount");
-						else if(events.getString("consumedAmount").equals("less than helf"))
+						else if(events.getString("consumedAmount").equals("less than half"))
 							counterWater += 0.4*events.getInt("amount");
 						else if(events.getString("consumedAmount").equals("nothing"))
 							counterWater += 0*events.getInt("amount");
@@ -91,29 +86,28 @@ public class Asker {
 				if(time == 1) {
 					if(counterWater<600) { 		// if he drink less than he actualy need near to the end of the day
 						try {
-							sender.send(kids.getInt("childID"),today[2]+"/"+today[1]+"/"+today[0],hour+":"+min+ ":" + sec,"לא תקין","Water", "!הילד לא שתה מספיק מים היום");
+							sender.send(kids.getInt("childID"),today[0]+"/"+today[1]+"/"+today[2],today[3]+":"+today[4]+ ":" + today[5],"לא תקין","Water", "הילד לא שתה מספיק מים היום!");
+							value++;
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} // the function that makes the alert
-						value++;
 					}else System.out.println(kids.getString("firstName")+ " "+counterWater); // no need actuely to pay attention for else cuz its mine everything ok
 						counterWater=0;
 				}
 				if(time == 2) {
 					if(counterWater<300) { 		// if he drink less than he actualy need in the half of the day
 						try {
-							sender.send(kids.getInt("childID"),today[2]+"/"+today[1]+"/"+today[0],hour+":"+min+ ":" + sec,"לא תקין","Water", "הילד לא שתה מספיק מים היום!");
+							sender.send(kids.getInt("childID"),today[0]+"/"+today[1]+"/"+today[2],today[3]+":"+today[4]+ ":" + today[5],"לא תקין","Water", "הילד לא שתה מספיק מים היום!");
+							value++;
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} // the function that makes the alert
-						value++;
 					}else System.out.println(kids.getString("firstName")+ " " + counterWater); // no need actuely to pay attention for else cuz its mine everything ok
 						counterWater=0;
 				}
 			}
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -123,20 +117,15 @@ public class Asker {
 	
 	//check the amount of water every child drink in the pass coupl days(3)
 	// needed to asked near to the end of the day
-	int XDaysEgoWaterChack(Connection myConn) { 
+	int XDaysEgoWaterChack(Connection myConn,String[] today) { 
 		int value=0;
 		try {
 			Statement mystmt = myConn.createStatement();
 			String giveMeAllKids= "SELECT * FROM child";
 			ResultSet kids= mystmt.executeQuery(giveMeAllKids);//sent the query to get all the kids
-			String [] today = LocalDate.now().toString().split("-");
-			String[] dateEvent,temp,nowtime;
-			int counterWater = 0,hour,min,sec;
-			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-			nowtime = timeStamp.split("_");									//getting the date and the time of the event a
-			 hour = (nowtime[1].charAt(0)-'0')*10 +nowtime[1].charAt(1)-'0';
-			 min = (nowtime[1].charAt(2)-'0')*10 +nowtime[1].charAt(3)-'0';
-			 sec = (nowtime[1].charAt(4)-'0')*10 +nowtime[1].charAt(5)-'0';
+			String[] dateEvent,temp;
+			int counterWater = 0;
+
 			while(kids.next()) {																			// pass all the kids 
 				//System.out.println(kids.getString("firstName") + "," + kids.getString("childID"));          // print the name of the kid and his ID
 				String giveMeAllEvents = "SELECT * FROM Water WHERE child = " + kids.getString("childID"); 	//the query to get all the event in Water for this kid
@@ -146,13 +135,13 @@ public class Asker {
 					dateEvent = events.getString("eventDate").split("/");									//getting the date and the time of the event a
 					temp = dateEvent[2].toString().split(",");
 					dateEvent[2]=temp[0];
-					if(today[1].equals(dateEvent[1])&& today[0].equals(dateEvent[2])&&(today[2].equals(dateEvent[0])||today[2].equals(dateEvent[0]+1)||today[2].equals(dateEvent[0]+2))) { //if the date of the event is today
-						//System.out.println(events.getString("child")+"-"+ events.getString("amount") + "-" + events.getString("consumedAmount") + "-" + events.getString("eventDate")); //print the id of the kid the amount of water he get and the amount he actualy drink
+					
+					if(today[1].equals(dateEvent[1])&& today[2].equals(dateEvent[2])&&(today[0].equals(dateEvent[0])||today[0].equals(dateEvent[0]+1)||today[0].equals(dateEvent[0]+2))) { //if the date of the event is what we looking for
 						if(events.getString("consumedAmount").equals("finish"))									//checking the amount he drink and sum the amount he drink all day
 							counterWater += events.getInt("amount");
-						else if(events.getString("consumedAmount").equals("more than helf"))
+						else if(events.getString("consumedAmount").equals("more than half"))
 							counterWater += 0.6*events.getInt("amount");
-						else if(events.getString("consumedAmount").equals("less than helf"))
+						else if(events.getString("consumedAmount").equals("less than half"))
 							counterWater += 0.4*events.getInt("amount");
 						else if(events.getString("consumedAmount").equals("nothing"))
 							counterWater += 0*events.getInt("amount");
@@ -160,12 +149,12 @@ public class Asker {
 				}
 					if(counterWater<1350) { 		// if he drink less than he actualy need near to the end of the day
 						try {
-							sender.send(kids.getInt("childID"),today[2]+"/"+today[1]+"/"+today[0],hour+":"+min+ ":" + sec,"לא תקין","Water", "הילד לא שתה מספיק מים היום!");
+							sender.send(kids.getInt("childID"),today[0]+"/"+today[1]+"/"+today[2],today[3]+":"+today[4]+ ":" + today[5],"לא תקין","Water", "הילד לא שותה מספיק מים !"); // the function that makes the alert
+							value++;
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
-						} // the function that makes the alert
-						value++;
+						} 
 					}
 					counterWater=0;
 			}
@@ -178,6 +167,87 @@ public class Asker {
 	}
 	
 	
+	
+	
+	int DailyFoodChack(Connection myConn,String[] today) {
+		int value=0;
+		try {
+			Statement mystmt = myConn.createStatement();
+			String giveMeAllKids= "SELECT * FROM child";
+			ResultSet kids= mystmt.executeQuery(giveMeAllKids);//sent the query to get all the kids
+			String[] dateEvent,temp;
+			float counterWater = 0;
+			while(kids.next()) {																				// pass each kid
+				//System.out.println(kids.getString("firstName") + "," + kids.getString("childID"));            // print the name of the kid
+				String giveMeAllEvents = "SELECT * FROM SolidFood WHERE child = " + kids.getString("childID"); 	//the query to get all the event in Water for this kid
+				Statement mystmt2 = myConn.createStatement(); 													//creating statement
+				ResultSet events = mystmt2.executeQuery(giveMeAllEvents);										//execute the query
+				while(events.next()) {																			//pass all the events
+					dateEvent = events.getString("eventDate").split("/");										//getting the date and the time of the event a
+					temp = dateEvent[2].toString().split(",");
+					dateEvent[2]=temp[0];
+					if(today[0].equals(dateEvent[0])&&today[1].equals(dateEvent[1])&& today[2].equals(dateEvent[2])) { //if the date of the event is today
+						System.out.println(events.getString("consumedAmount")); //print the id of the kid the amount of water he get and the amount he actualy drink
+						if(events.getString("consumedAmount").equals("finish"))	{								//checking the amount he drink and sum the amount he drink all day
+							counterWater += 1;}
+						else if(events.getString("consumedAmount").equals("more than half")) {
+							counterWater += 0.6;}
+						else if(events.getString("consumedAmount").equals("less than half")) {
+							counterWater += 0.4;}
+						else if(events.getString("consumedAmount").equals("nothing")) {
+							counterWater += 0;}
+					}					
+				}
+				// need to know what is the real amout of food that need to count
+				String giveMeAllEvents2 = "SELECT * FROM LiquidFood WHERE child = " + kids.getString("childID"); 	//the query to get all the event in Water for this kid
+				Statement mystmt3 = myConn.createStatement(); 													//creating statement
+				ResultSet events2 = mystmt3.executeQuery(giveMeAllEvents2);										//execute the query
+				while(events2.next()) {
+					dateEvent = events2.getString("eventDate").split("/");										//getting the date and the time of the event a
+					temp = dateEvent[2].toString().split(",");
+					dateEvent[2]=temp[0];
+					if(today[0].equals(dateEvent[0])&&today[1].equals(dateEvent[1])&& today[2].equals(dateEvent[2])) {  //if the date of the event is today
+						System.out.println(events2.getString("child")+ "-" + events2.getString("consumedAmount") + "-" + events2.getString("eventDate")+ " L"); //print the id of the kid the amount of water he get and the amount he actualy drink
+						if(events2.getString("consumedAmount").equals("finish")) {									//checking the amount he drink and sum the amount he drink all day
+							counterWater += 1;}
+						else if(events2.getString("consumedAmount").equals("more than half")) {
+							counterWater += 0.6;}
+						else if(events2.getString("consumedAmount").equals("less than half")) {
+							counterWater += 0.4;}
+						else if(events2.getString("consumedAmount").equals("nothing")) {
+							counterWater += 0;}
+						
+					}
+				}
+				System.out.println(counterWater);
+					if(counterWater< 1.0) { 		// if he ate less than he actually need near to the end of the day
+						try {
+							sender.send(kids.getInt("childID"),today[0]+"/"+today[1]+"/"+today[2],today[3]+":"+today[4]+ ":" + today[5],"לא תקין","Food","הילד לא אכל היום מספיק!"); // the function that makes the alert
+							value++;
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+					}
+				}else System.out.println(kids.getString("firstName")+ " "+counterWater); // no need actuely to pay attention for else cuz its mean everything ok
+					counterWater=0;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//function that get time of start and the end and check if now is between  them
 	boolean checkTime(int starthour,int startmin,int finishhour,int finishmin) { 
 		int hour,min; 
@@ -188,7 +258,7 @@ public class Asker {
 		time = timeStamp.split("_");	
 		 hour = (time[1].charAt(0)-'0')*10 +time[1].charAt(1)-'0';
 		 min = (time[1].charAt(2)-'0')*10 +time[1].charAt(3)-'0';
-		 if(((hour > starthour) && (hour < finishhour))||((hour == starthour&&min>=startmin) && (hour == finishhour&&min<=finishmin))||((hour == starthour&&min>=startmin) && (hour >= finishhour))||((hour > starthour) && (hour == finishhour&&min<=finishmin))) {
+		 if(((hour > starthour) && (hour < finishhour))||((hour == starthour&&min>=startmin) && (hour == finishhour&&min<=finishmin))||((hour == starthour&&min>=startmin) &&(starthour!=finishhour) &&(hour <= finishhour))||((hour > starthour) && (hour == finishhour&&min<=finishmin))) {
 		return true;
 		}
 		 else return false;
@@ -201,5 +271,23 @@ public class Asker {
 		sdf.applyPattern("EEE");
 		String sMyDate = sdf.format(myDate);
 		return sMyDate;
+	}
+	
+	String[] WhatIsTheDate() {
+		String [] today = LocalDate.now().toString().split("-"),nowtime,now = null;
+		String hour,min,sec;
+		now= new String[6];
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+		nowtime = timeStamp.split("_");									//getting the date and the time of the event 
+		 hour = nowtime[1].charAt(0) + "" + nowtime[1].charAt(1);
+		 min =  nowtime[1].charAt(2) + "" + nowtime[1].charAt(3);
+		 sec =  nowtime[1].charAt(4) + "" + nowtime[1].charAt(5);
+		now[0] = today[2];
+		now[1] = today[1];
+		now[2] = today[0];
+		now[3] = hour;
+		now[4] = min;
+		now[5] = sec;
+		return now;
 	}
 }
